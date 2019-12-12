f61a14036b8bc36931ba2892163cd360515cd2d6
joe zhang <joe.zhang@ringcentral.com>
2019-12-12 17:20:03 +0800

    springboot logstash

---
+++ springboot-logstash/src/main/java/com/springboot/example/model/User.java
@@ -0,0 +1,31 @@
+package com.springboot.example.model;
+
+public class User {
+
+    private String name;
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
