ad240a087d822b69cb1007db28d58477b45f84e5
joe zhang <joe.zhang@ringcentral.com>
2020-08-15 10:30:18 +0800

    add kafka config

---
+++ spring-boot-kafka/src/main/java/com/example/boot/kafka/BootKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootKafkaApplication.class, args);
+    }
+}
---
+++ spring-boot-kafka/src/main/java/com/example/boot/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,7 @@
+package com.example.boot.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+}
