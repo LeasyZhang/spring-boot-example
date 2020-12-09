a9099027e9618dde90f2a1cb74d70fb4b9bb28cd
joe zhang <joe.zhang@ringcentral.com>
2020-12-10 01:02:47 +0800

    how to enable skywalking

--- spring-boot-web-example/src/main/java/com/springboot/example/WebBootApplication.java
+++ spring-boot-web-example/src/main/java/com/springboot/example/WebBootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootApplication {
+public class WebBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+        SpringApplication.run(WebBootApplication.class, args);
     }
 }
