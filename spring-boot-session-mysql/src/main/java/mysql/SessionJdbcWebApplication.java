3edef249ac7bc2f9250eacf9fef8747eec90eb29
joe zhang <joe.zhang@ringcentral.com>
2021-01-20 21:42:34 +0800

    spring session mysql

---
+++ spring-boot-session-mysql/src/main/java/mysql/SessionJdbcWebApplication.java
@@ -0,0 +1,12 @@
+package mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionJdbcWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionJdbcWebApplication.class, args);
+    }
+}
