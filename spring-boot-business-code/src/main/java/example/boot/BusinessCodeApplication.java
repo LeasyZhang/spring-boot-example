0cb9c53d02010c05909de0dd5c5d2201a4b9a3a0
joe zhang <joe.zhang@ringcentral.com>
2021-02-21 17:25:40 +0800

    spring business code action

---
+++ spring-boot-business-code/src/main/java/example/boot/BusinessCodeApplication.java
@@ -0,0 +1,12 @@
+package example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessCodeApplication.class, args);
+    }
+}
