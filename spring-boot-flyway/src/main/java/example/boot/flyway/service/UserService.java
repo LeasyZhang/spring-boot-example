ce08b205382a355c9b19bb3995575c02bc325999
joe zhang <joe.zhang@ringcentral.com>
2021-04-13 22:06:49 +0800

    flyway

--- spring-boot-flyway/src/main/java/example/boot/flyway/service/UserService.java
+++ spring-boot-flyway/src/main/java/example/boot/flyway/service/UserService.java
@@ -1,4 +1,18 @@
 package example.boot.flyway.service;
 
+import example.boot.flyway.model.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    int create(String name, Integer age);
+
+    List<User> getByName(String name);
+
+    int deleteByName(String name);
+
+    int getAllUsers();
+
+    int deleteAllUsers();
 }
